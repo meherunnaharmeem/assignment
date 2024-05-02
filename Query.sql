ce8fbca8e2ead51a7d92233ca8df5aee3e43bd2d
meherunnaharmeem <2221081043@uttarauniversity.edu.bd>
2024-05-02 12:48:22 +0600

    first commit

---
+++ Query.sql
@@ -0,0 +1,8 @@
+USE student
+CREATE TABLE Student1(
+	id INT PRIMARY KEY,
+	NAME VARCHAR (55),
+	address VARCHAR (66));
+INSERT INTO Student1 (id ,NAME,address) VALUES 
+(1,'shihab','uttara'),
+(2,'mim','tongi');
